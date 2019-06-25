130d4ea0df850f1fe3a93360cb4941db48f0f323
yintianyu <eecsyty@outlook.com>
2019-06-25 19:18:05 +0800

    Pass Lab2B

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -45,6 +45,14 @@ type ApplyMsg struct {
 }
 
 //
+// A Go struct implementing log entry
+//
+type LogEntry struct {
+	Command      interface{}
+	TermReceived int
+}
+
+//
 // A Go object implementing a single Raft peer.
 //
 type Raft struct {
@@ -56,15 +64,18 @@ type Raft struct {
 	// Your data here (2A, 2B, 2C).
 	// Look at the paper's Figure 2 for a description of what
 	// state a Raft server must maintain.
-	state       int      // state machine : 0 - leader 1 - follwer 2 - candidate
-	currentTerm int      // lastest term server has seen(initialized to 0)
-	votedFor    int      // candidateId that received vote in current term(or null if none)
-	log         []string // log entries; each entry contains command for state machine, and term when entry was recieved by leader
-	commitIndex int      // index of highest log entry known to be commited(initialized to 0)
-	lastApplied int      // index of highest log entry applied to state machine(initialized to 0)
+	state       int        // state machine : 0 - leader 1 - follwer 2 - candidate
+	currentTerm int        // lastest term server has seen(initialized to 0)
+	votedFor    int        // candidateId that received vote in current term(or null if none)
+	log         []LogEntry // log entries; each entry contains command for state machine, and term when entry was recieved by leader
+	commitIndex int        // index of highest log entry known to be commited(initialized to 0)
+	lastApplied int        // index of highest log entry applied to state machine(initialized to 0)
+	nextIndex   []int      // for each server, index of the next log entry to send to that server (initialized to leader last log index + 1)
+	matchIndex  []int      // for each server index of highest log entry known to be replicated on server (initialized to 0, increases monotonically)
 
 	voteTimer *time.Timer // vote timer
 	mux       sync.Mutex
+	applyCh   *chan ApplyMsg
 }
 
 // return currentTerm and whether this server
@@ -147,12 +158,12 @@ type RequestVoteReply struct {
 //
 type AppendEntriesArgs struct {
 	// Your data here.
-	Term         int      // leader's term
-	LeaderId     int      // so follower can redirect clients
-	PrevLogIndex int      // index of log entry immediately preceding new ones
-	PrevLogTerm  int      // term of prevLogIndex entry
-	Entries      []string // log entries to store(empty for heartbeat; may send more than one for efficiency)
-	LeaderCommit int      // leader's commitIndex
+	Term         int        // leader's term
+	LeaderId     int        // so follower can redirect clients
+	PrevLogIndex int        // index of log entry immediately preceding new ones
+	PrevLogTerm  int        // term of prevLogIndex entry
+	Entries      []LogEntry // log entries to store(empty for heartbeat; may send more than one for efficiency)
+	LeaderCommit int        // leader's commitIndex
 }
 
 //
@@ -172,8 +183,16 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	// Your code here (2A, 2B).
 	rf.mux.Lock()
 	if rf.currentTerm < args.Term {
+		// DPrintf("[Server %d] VoteRPC len(rf.log)=%v, args.LastLogIndex=%v, args.LastLogTerm=%v, localLast=%v\n",
+		// rf.me, len(rf.log), args.LastLogIndex, args.LastLogTerm, rf.log[len(rf.log)-1].TermReceived)
 		rf.currentTerm = args.Term
 		rf.state = 1
+		if rf.log[len(rf.log)-1].TermReceived > args.LastLogTerm || (rf.log[len(rf.log)-1].TermReceived == args.LastLogTerm && len(rf.log)-1 > args.LastLogIndex) {
+			reply.Term = rf.currentTerm
+			reply.VoteGranted = false
+			rf.mux.Unlock()
+			return
+		}
 		rf.votedFor = args.CandidateId
 		reply.VoteGranted = true
 		reply.Term = rf.currentTerm
@@ -182,7 +201,7 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	} else {
 		reply.Term = rf.currentTerm
 		reply.VoteGranted = false
-		DPrintf("[Server %d] Reject %d at Term %d\n", rf.me, args.CandidateId, rf.currentTerm)
+		// DPrintf("[Server %d] Reject %d at Term %d\n", rf.me, args.CandidateId, rf.currentTerm)
 	}
 	rf.mux.Unlock()
 }
@@ -194,17 +213,44 @@ func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply
 	// Your code here.
 	rf.mux.Lock()
 	if rf.currentTerm <= args.Term {
+		if rf.currentTerm < args.Term {
+			DPrintf("[Server %d] get higher term %v from %v at term %v\n", rf.me, args.Term, args.LeaderId, rf.currentTerm)
+		}
 		rf.currentTerm = args.Term
 		reply.Term = args.Term
 		reply.Success = true
 		rf.state = 1
-		DPrintf("[Server %d] in appendEntries handler SUCCESS at Term %d\n", rf.me, args.Term)
+		if len(rf.log)-1 < args.PrevLogIndex {
+			reply.Success = false
+		} else if rf.log[args.PrevLogIndex].TermReceived != args.PrevLogTerm {
+			reply.Success = false
+		} else {
+			oldPst := args.PrevLogIndex + 1
+			newPst := 0
+			for oldPst < len(rf.log) && newPst < len(args.Entries) {
+				if rf.log[oldPst].TermReceived != args.Entries[newPst].TermReceived {
+					break
+				}
+				oldPst++
+				newPst++
+			}
+			rf.log = append(rf.log[0:oldPst], args.Entries[newPst:]...)
+			DPrintf("[Server %d] accept logs idx [%v, %v] from %v, at term=%v\n", rf.me, oldPst, len(rf.log)-1, args.LeaderId, rf.currentTerm)
+			if args.LeaderCommit > rf.commitIndex {
+				if args.LeaderCommit < len(rf.log)-1 {
+					rf.commitIndex = args.LeaderCommit
+				} else {
+					rf.commitIndex = len(rf.log) - 1
+				}
+			}
+		}
+		// DPrintf("[Server %d] in appendEntries handler SUCCESS at Term %d\n", rf.me, args.Term)
 		rf.mux.Unlock()
 		rf.voteTimerReset()
 	} else {
 		reply.Success = false
 		reply.Term = rf.currentTerm
-		DPrintf("[Server %d] in appendEntries handler FAIL at Term %d\n", rf.me, reply.Term)
+		// DPrintf("[Server %d] in appendEntries handler FAIL at Term %d\n", rf.me, reply.Term)
 		rf.mux.Unlock()
 	}
 }
@@ -271,6 +317,19 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	isLeader := true
 
 	// Your code here (2B).
+	rf.mux.Lock()
+	if rf.state != 0 {
+		rf.mux.Unlock()
+		return -1, -1, false
+	}
+	index = len(rf.log)
+	term = rf.currentTerm
+	var newLogEntry LogEntry
+	newLogEntry.Command = command
+	newLogEntry.TermReceived = rf.currentTerm
+	rf.log = append(rf.log, newLogEntry)
+	DPrintf("[Server %d] Start append %v, term=%v\n", rf.me, len(rf.log)-1, rf.currentTerm)
+	rf.mux.Unlock()
 
 	return index, term, isLeader
 }
@@ -291,12 +350,12 @@ func (rf *Raft) voteTimerCheck() {
 		<-rf.voteTimer.C
 		rf.mux.Lock()
 		if rf.state != 0 {
-			DPrintf("[Server %d] vote Timer expires at Term %d\n", rf.me, rf.currentTerm)
+			// DPrintf("[Server %d] vote Timer expires at Term %d\n", rf.me, rf.currentTerm)
 			rf.state = 2 // turn to candidate
 			rf.mux.Unlock()
 			go rf.issueVote()
 		} else {
-			DPrintf("[Server %d] vote Timer expires at Term %d, but a leader\n", rf.me, rf.currentTerm)
+			// DPrintf("[Server %d] vote Timer expires at Term %d, but a leader\n", rf.me, rf.currentTerm)
 			rf.mux.Unlock()
 		}
 		rf.voteTimerReset()
@@ -323,6 +382,8 @@ func (rf *Raft) issueVote() {
 	var args RequestVoteArgs
 	args.Term = rf.currentTerm
 	args.CandidateId = rf.me
+	args.LastLogIndex = len(rf.log) - 1
+	args.LastLogTerm = rf.log[len(rf.log)-1].TermReceived
 	rf.votedFor = rf.me
 	rf.mux.Unlock()
 	count := 1
@@ -334,17 +395,17 @@ func (rf *Raft) issueVote() {
 				ok := rf.sendRequestVote(i, &args, &reply)
 				if !ok {
 					voteMeChan <- false
-					DPrintf("[Server %d] requestVote to %d return failed at Term %d, granted = %v\n", rf.me, i, rf.currentTerm, reply.VoteGranted)
+					// DPrintf("[Server %d] requestVote to %d return failed at Term %d, granted = %v\n", rf.me, i, rf.currentTerm, reply.VoteGranted)
 				} else {
 					if reply.VoteGranted {
 						voteMeChan <- true
-						DPrintf("[Server %d] %d voted to me at Term %d\n", rf.me, i, reply.Term)
+						// DPrintf("[Server %d] %d voted to me at Term %d\n", rf.me, i, reply.Term)
 					} else {
 						voteMeChan <- false
 						rf.mux.Lock()
-						DPrintf("[Server %d] %d didn't vote to me at Term %d\n", rf.me, i, reply.Term)
+						// DPrintf("[Server %d] %d didn't vote to me at Term %d\n", rf.me, i, reply.Term)
 						if reply.Term > rf.currentTerm {
-							DPrintf("[Server %d] Term is corrected by %d during election\n", rf.me, i)
+							// DPrintf("[Server %d] Term is corrected by %d during election\n", rf.me, i)
 							rf.currentTerm = reply.Term
 							rf.state = 1
 						}
@@ -356,7 +417,7 @@ func (rf *Raft) issueVote() {
 	}
 	for i := 0; i < len(rf.peers)-1; i++ {
 		voteMe := <-voteMeChan
-		DPrintf("[Server %d] got vote No.%d, %v\n", rf.me, i, voteMe)
+		// DPrintf("[Server %d] got vote No.%d, %v\n", rf.me, i, voteMe)
 		if voteMe {
 			count++
 		}
@@ -364,15 +425,19 @@ func (rf *Raft) issueVote() {
 			break
 		}
 	}
-	DPrintf("[Server %d] count = %d at Term %d BEFORE LOCK\n", rf.me, count, rf.currentTerm)
+	// DPrintf("[Server %d] count = %d at Term %d BEFORE LOCK\n", rf.me, count, rf.currentTerm)
 	rf.mux.Lock()
-	DPrintf("[Server %d] count = %d at Term %d AFTER LOCK\n", rf.me, count, rf.currentTerm)
+	// DPrintf("[Server %d] count = %d at Term %d AFTER LOCK\n", rf.me, count, rf.currentTerm)
 	if rf.state == 2 && count > len(rf.peers)/2 {
 		rf.state = 0 // wins
+		for i, _ := range rf.peers {
+			rf.nextIndex[i] = len(rf.log)
+			rf.matchIndex[i] = 0
+		}
 		DPrintf("[Server %d] becomes leader at Term %d\n", rf.me, rf.currentTerm)
 	} else if rf.state == 2 {
 		rf.state = 1 // fails
-		DPrintf("[Server %d] fails the election at Term %d\n", rf.me, rf.currentTerm)
+		// DPrintf("[Server %d] fails the election at Term %d\n", rf.me, rf.currentTerm)
 	}
 	rf.mux.Unlock()
 }
@@ -387,20 +452,39 @@ func (rf *Raft) leaderHeartbeat() {
 				var args AppendEntriesArgs
 				var reply AppendEntriesReply
 				rf.mux.Lock()
+				if rf.state != 0 {
+					rf.mux.Unlock()
+					return
+				}
 				args.Term = rf.currentTerm
 				args.LeaderId = rf.me
+				args.PrevLogIndex = rf.nextIndex[i] - 1 // TODO check this
+				args.PrevLogTerm = rf.log[args.PrevLogIndex].TermReceived
+				if len(rf.log)-1 >= rf.nextIndex[i] {
+					args.Entries = rf.log[rf.nextIndex[i]:]
+				}
+				args.LeaderCommit = rf.commitIndex
 				rf.mux.Unlock()
 				ok := rf.sendAppendEntries(i, &args, &reply)
 				if ok {
 					rf.mux.Lock()
-					if reply.Term > rf.currentTerm {
-						DPrintf("[Server %d] Term %d -> %d during heartbeat, convert to follower\n", rf.me, rf.currentTerm, reply.Term)
+					if reply.Success {
+						rf.matchIndex[i] = args.PrevLogIndex + len(args.Entries)
+						rf.nextIndex[i] = args.PrevLogIndex + len(args.Entries) + 1
+					} else if reply.Term > rf.currentTerm {
+						// DPrintf("[Server %d] Term %d -> %d during heartbeat, convert to follower\n", rf.me, rf.currentTerm, reply.Term)
 						rf.currentTerm = reply.Term
 						rf.state = 1
+					} else {
+						rf.nextIndex[i]--
 					}
 					rf.mux.Unlock()
 				}
 			}(i)
+		} else {
+			rf.mux.Lock()
+			rf.matchIndex[i] = len(rf.log) - 1
+			rf.mux.Unlock()
 		}
 	}
 }
@@ -426,6 +510,14 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	// Your initialization code here (2A, 2B, 2C).
 	rf.state = 1 // initially follower
 	rf.currentTerm = 0
+	rf.commitIndex = 0
+	rf.lastApplied = 0
+
+	var psudoEntry LogEntry
+	psudoEntry.TermReceived = 0
+	rf.log = append(rf.log, psudoEntry)
+	rf.nextIndex = make([]int, len(peers))
+	rf.matchIndex = make([]int, len(peers))
 
 	d := rand.Int31n(150) + 500
 	rf.voteTimer = time.NewTimer(time.Duration(d) * time.Millisecond)
@@ -434,6 +526,7 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	// behavior
 	go func() {
 		for {
+			time.Sleep(time.Duration(10) * time.Millisecond)
 			rf.mux.Lock()
 			if rf.state == 2 {
 				rf.mux.Unlock()
@@ -443,15 +536,44 @@ func Make(peers []*labrpc.ClientEnd, me int,
 				time.Sleep(time.Duration(150) * time.Millisecond)
 				rf.mux.Lock()
 				if rf.state == 0 {
+					// Commit TODO Optimize this
+					for N := rf.commitIndex + 1; N < len(rf.log); N++ {
+						if rf.log[N].TermReceived < rf.currentTerm {
+							continue
+						}
+						cnt := 0
+						for i := 0; i < len(rf.peers); i++ {
+							// DPrintf("[Server %d Leader] matchIndex[%v]=%v\n", rf.me, i, rf.matchIndex[i])
+							if rf.matchIndex[i] >= N {
+								cnt++
+							}
+						}
+						if cnt > len(rf.peers)/2 && rf.log[N].TermReceived == rf.currentTerm {
+							rf.commitIndex = N
+						} else {
+							break
+						}
+					}
+					DPrintf("[Server %d Leader] commitIndex=%v, logLen=%v, term=%v\n", rf.me, rf.commitIndex, len(rf.log), rf.currentTerm)
 					rf.mux.Unlock()
 					rf.leaderHeartbeat()
 				} else {
 					rf.mux.Unlock()
 				}
-				// TODO: Send AppendEntries
 			} else {
 				rf.mux.Unlock()
 			}
+			rf.mux.Lock()
+			for rf.lastApplied < rf.commitIndex {
+				rf.lastApplied++
+				var msg ApplyMsg
+				msg.Command = rf.log[rf.lastApplied].Command
+				msg.CommandIndex = rf.lastApplied
+				msg.CommandValid = true
+				applyCh <- msg
+				DPrintf("[Server %d] Commit %v, index = %v, term = %v\n", rf.me, msg.Command, msg.CommandIndex, rf.currentTerm)
+			}
+			rf.mux.Unlock()
 		}
 	}()
 
